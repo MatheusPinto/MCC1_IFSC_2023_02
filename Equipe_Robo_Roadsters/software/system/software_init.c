0433fef05b57dbb6595fb2fe64a46a92818f4eae
Bruno Camargo <84508340+nobrucamargo@users.noreply.github.com>
2023-12-10 18:49:57 -0300

    Update software_init.c

--- Equipe_Robo_Roadsters/software/system/software_init.c
+++ Equipe_Robo_Roadsters/software/system/software_init.c
@@ -9,13 +9,6 @@
 
 void System_Software_Init( void )
 {
-	// Inicialização dos pinos de TX e RX
-	PORT_Init(PORTB);
-	PORT_SetMux( PORTB, UART_TX, PORT_MUX_ALT2 );
-	PORT_SetMux( PORTB, UART_RX, PORT_MUX_ALT2 );
-
-	UART_SetClkSrc( UART0, UART_CLOCK_FLL ); // Setando o clk src do módulo UART(memso clk src do PWM)
-	UART_Init( UART0, BAUD_RATE_HC05, UART_TX_RX_ENABLE, UART_NO_PARITY, UART_ONE_STOP_BIT );
 }
 
 
